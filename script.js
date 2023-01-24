1eb20a2150ddf1c57ebae1299f3a73f05a62bbb3
Matthew Swineford <m.swineford@yahoo.com>
2023-01-24 12:49:05 -0500

    added questions to the questions array

--- script.js
+++ script.js
@@ -26,6 +26,46 @@ var finishLine = document.getElementById("finish");
 var scoreName = document.getElementById("scoreInitials");
 var scoreTotal = document.getElementById("finalScore");
 
-var questionsArray = [{
-    
-}]
+// questions
+var questionsArray = [
+    {
+        question: "Commonly used datatypes DO NOT include ___.",
+        answerA: "strings",
+        answerB: "booleans",
+        answerC: "alerts",
+        answerD: "numbers",
+        correct: "a"
+    },
+    {
+        question: "Arrays in javascript can be used to store ___",
+        answerA: "numbers and strings",
+        answerB: "other arrays",
+        answerC: "booleans",
+        answerD: "all of the above",
+        correct: "d"
+    },
+    {
+        question: "String values must be enclosed within ___ when assigned to variables.",
+        answerA: "commas",
+        answerB: "curly braces",
+        answerC: "quotes",
+        answerD: "square brackets",
+        correct: "b"
+    },
+    {
+        question: "String values must be enclosed in ___.",
+        answerA: "double quotes",
+        answerB: "single quotes",
+        answerC: "both a and b",
+        answerD: "none of the above",
+        correct: "c"
+    },
+    {
+        question: "The condition of an if / else statement is closed within ___",
+        answerA: "quotes",
+        answerB: "curly brackets",
+        answerC: "parentheses",
+        answerD: "square bracketts",
+        correct: "c"
+    }
+];
