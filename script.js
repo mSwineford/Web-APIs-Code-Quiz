04af070360b07421c1aa8ff12e7a4e6746480977
Matthew Swineford <m.swineford@yahoo.com>
2023-01-24 11:13:57 -0500

    made html and made variables to go with it

---
+++ script.js
@@ -0,0 +1,26 @@
+// menue variables
+var startMenu = document.getElementById("start");
+var startButton = document.getElementById("startbtn");
+
+// variables that make up the quiz itself
+var theQuiz = document.getElementById("quiz");
+var stopWatch = document.getElementById("clock");
+var quizQuestions = document.getElementById("questions");
+var solutions = document.getElementById("solution");
+// choices
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
+
+// results
+var gameOver = document.getElementById("gameOver");
+var grades = document.getElementById("grade");
+var inputInitials = document.getElementById("initials");
+var saveButton = document.getElementById("saveScore");
+
+// score board
+var scoreContainers = document.getElementById("scoreContainer");
+var scoreBoard = document.getElementById("scoreBoard");
+var finishLine = document.getElementById("finish");
+var scoreTotal = document.getElementById("finalScore");
